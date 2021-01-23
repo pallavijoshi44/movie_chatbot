139dd55d7573069b639e67451488b272fd5b190d
Pallavi <pallavijoshi4@gmail.com>
2021-01-23 12:11:40 +0100

    Replace widgets with ListTile

--- lib/widget/carousel_dialog_slider.dart
+++ lib/widget/carousel_dialog_slider.dart
@@ -20,109 +20,92 @@ class _CarouselDialogSliderState extends State<CarouselDialogSlider> {
     final List<String> imageList =
         widget.carouselSelect.items.map((item) => item.image.imageUri).toList();
     return Container(
-      margin: const EdgeInsets.only(top: 10.0),
+        margin: const EdgeInsets.only(top: 10.0),
         child: Column(
-      children: <Widget>[
-        CarouselSlider(
-          options: CarouselOptions(
-            autoPlay: true,
-            aspectRatio: 1.0,
-            enlargeCenterPage: true,
-            enableInfiniteScroll: false,
-            enlargeStrategy: CenterPageEnlargeStrategy.height
-          ),
-          items: imageList
-              .map((item) => InkWell(
-                    onTap: () async {
-                      // var permission = await Geolocator.checkPermission();
-                      var currentPosition =
-                          await Geolocator.getCurrentPosition();
-                      var placeMarks = await placemarkFromCoordinates(
-                          currentPosition.latitude, currentPosition.longitude);
-                      var _countryCode = 'US';
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(
+                  autoPlay: true,
+                  aspectRatio: 1.0,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height),
+              items: imageList
+                  .map((item) => InkWell(
+                        onTap: () async {
+                          var currentPosition =
+                              await Geolocator.getCurrentPosition();
+                          var placeMarks = await placemarkFromCoordinates(
+                              currentPosition.latitude,
+                              currentPosition.longitude);
+                          var _countryCode = 'US';
 
-                      if (placeMarks != null && placeMarks.length > 0) {
-                        _countryCode = placeMarks[0].isoCountryCode;
-                      }
-                      var movieTitle = widget
-                          .carouselSelect.items[imageList.indexOf(item)].title;
-                      widget.carouselItemClicked(
-                          _countryCode,
-                          widget.carouselSelect.items[imageList.indexOf(item)]
-                              .info['key'],
-                          movieTitle.substring(0, movieTitle.indexOf(' (')));
-                      // return _inform(context,
-                      // '${widget.carouselSelect.items[imageList.indexOf(item)].title}');
-                    },
-                    child: Card(
-                      elevation: 5,
-                      margin: EdgeInsets.all(5),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      child: Column(
-                        children: [
-                          Flexible(
-                            child: Container(
-                              child: ClipRRect(
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(15),
-                                      topRight: Radius.circular(15)),
-                                  child: item.contains('null')
-                                      ? Image.asset(
-                                          'assets/images/placeholder.png',
-                                          width: 1000.0,
-                                          fit: BoxFit.cover,
-                                        )
-                                      : Image.network(item,
-                                          fit: BoxFit.cover, width: 1000.0)),
-                            ),
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
+                          if (placeMarks != null && placeMarks.length > 0) {
+                            _countryCode = placeMarks[0].isoCountryCode;
+                          }
+                          var movieTitle = widget.carouselSelect
+                              .items[imageList.indexOf(item)].title;
+                          widget.carouselItemClicked(
+                              _countryCode,
+                              widget.carouselSelect
+                                  .items[imageList.indexOf(item)].info['key'],
+                              movieTitle.substring(
+                                  0, movieTitle.indexOf(' (')));
+                        },
+                        child: Card(
+                          elevation: 5,
+                          margin: EdgeInsets.all(5),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Flexible(
+                                child: ClipRRect(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(15.0),
+                                      topRight: Radius.circular(15.0),
+                                    ),
+                                    child: item.contains('null')
+                                        ? Image.asset(
+                                            'assets/images/placeholder.png',
+                                            fit: BoxFit.cover,
+                                          )
+                                        : Image.network(item,
+                                             fit: BoxFit.scaleDown)),
                               ),
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 20.0),
-                              child: Text(
-                                '${widget.carouselSelect.items[imageList.indexOf(item)].title}',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
+                              ListTile(
+                                title: Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 5.0),
+                                  child: Text(
+                                    '${widget.carouselSelect.items[imageList.indexOf(item)].title}',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                subtitle: Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 5.0),
+                                  child: Text(
+                                    '${widget.carouselSelect.items[imageList.indexOf(item)].description ?? ""}',
+                                    maxLines: 5,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 15.0,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
+                            ],
                           ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(15),
-                                      bottomRight: Radius.circular(15))),
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 20.0),
-                              child: Text(
-                                '${widget.carouselSelect.items[imageList.indexOf(item)].description ?? ""}',
-                                maxLines: 5,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 15.0,
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ))
-              .toList(),
-        ),
-      ],
-    ));
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ],
+        ));
   }
 }

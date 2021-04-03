795c33861e94341de73450d5b9b5546b9eead39c
Pallavi <pallavijoshi4@gmail.com>
2021-04-03 18:05:06 +0200

    Fix ui for quick reply in android

--- lib/src/ui/quick_reply.dart
+++ lib/src/ui/quick_reply.dart
@@ -12,64 +12,38 @@ class QuickReply extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-      Container(
-        margin: const EdgeInsets.only(top: 15.0),
-        padding: const EdgeInsets.only(top: 15.0),
-        child: CircleAvatar(
+    return Container(
+      padding: const EdgeInsets.only(top: 15.0),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+        CircleAvatar(
           child: Text(''),
           backgroundColor: Colors.transparent,
         ),
-      ),
-      Expanded(
-        child: Wrap(
-            alignment: WrapAlignment.spaceEvenly,
-            direction: Axis.horizontal,
-            children: quickReplies.map((quickReply) {
-              return Container(
-                  margin: const EdgeInsets.all(10),
-                  child: OutlineButton(
-                    disabledBorderColor: Colors.grey[600],
-                    disabledTextColor: Colors.grey[900],
-                    child: Text(quickReply,
-                        style: TextStyle(fontFamily: 'OpenSans', fontSize: 14)),
-                    borderSide: BorderSide(color: Colors.lightGreen[900]),
-                    textColor: Colors.lightGreen[900],
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    onPressed: () {
-                      return insertQuickReply(quickReply);
-                    },
-                  ));
-            }).toList()),
-      ),
-    ]);
+        Expanded(
+          child: Wrap(
+              alignment: WrapAlignment.spaceAround,
+              runSpacing: 10,
+              direction: Axis.horizontal,
+              children: quickReplies.map((quickReply) {
+                return Container(
+                    child: OutlineButton(
+                      disabledBorderColor: Colors.grey[600],
+                      disabledTextColor: Colors.grey[900],
+                      child: Text(quickReply,
+                          style: TextStyle(fontFamily: 'OpenSans', fontSize: 14)),
+                      borderSide: BorderSide(color: Colors.lightGreen[900]),
+                      textColor: Colors.lightGreen[900],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      onPressed: () {
+                        return insertQuickReply(quickReply);
+                      },
+                    ));
+              }).toList()),
+        ),
+      ]),
+    );
     //  BotChatMessage(text
   }
 
-  Widget _buildWidget(String quickReply) {
-    return Platform.isIOS
-        ? CupertinoButton(
-            disabledColor: Colors.grey[600],
-            color: Colors.lightGreen[900],
-            child: Text(quickReply,
-                style: TextStyle(fontFamily: 'OpenSans', fontSize: 14)),
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            onPressed: () {
-              return insertQuickReply(quickReply);
-            })
-        : OutlineButton(
-            disabledBorderColor: Colors.grey[600],
-            disabledTextColor: Colors.grey[900],
-            child: Text(quickReply,
-                style: TextStyle(fontFamily: 'OpenSans', fontSize: 14)),
-            borderSide: BorderSide(color: Colors.lightGreen[900]),
-            textColor: Colors.lightGreen[900],
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15))),
-            onPressed: () {
-              return insertQuickReply(quickReply);
-            },
-          );
-  }
 }

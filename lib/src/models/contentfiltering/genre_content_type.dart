b9765f0b8055c6cd9253c96c50b906859201297d
Pallavi <pallavijoshi4@gmail.com>
2021-05-13 10:36:50 +0200

    fix genres for tv

--- lib/src/models/contentfiltering/genre_content_type.dart
+++ lib/src/models/contentfiltering/genre_content_type.dart
@@ -1,11 +1,11 @@
 class GenresContentType {
   static const movieGenresGroup1 = [ACTION, ADVENTURE, CRIME];
-  static const tvGenresGroup1 = [TV_ACTION_ADVENTURE_GENRE, WAR, MYSTERY];
+  static const tvGenresGroup1 = [TV_ACTION_ADVENTURE_GENRE, CRIME, MYSTERY, TV_SCI_FI_AND_FANTASY];
   static const tvMovieGroup1 = [TV_ACTION_ADVENTURE_GENRE, CRIME,  WAR, MYSTERY];
   static const movieTvGroup1 = [ACTION, ADVENTURE, CRIME,  WAR, MYSTERY];
 
   static const movieGenresGroup2 = [THRILLER, MYSTERY, HORROR];
-  static const tvGenresGroup2 = [MYSTERY, SCI_FI, FANTASY];
+  static const tvGenresGroup2 = [MYSTERY, TV_SCI_FI_AND_FANTASY, CRIME];
   static const tvMovieGroup2 = [MYSTERY, SCI_FI, FANTASY];
   static const movieTvGroup2 = [THRILLER, MYSTERY, HORROR];
 
@@ -21,7 +21,7 @@ class GenresContentType {
 
   static const movieGenresGroup5 = [DOCUMENTARY, WAR, WESTERN, HISTORY];
   static const tvGenresGroup5 = [DOCUMENTARY, TV_NEWS, TV_REALITY, TV_TALK];
-  static const tvMovieGroup5 = [DOCUMENTARY, TV_WAR_POLITICS,WESTERN ];
+  static const tvMovieGroup5 = [DOCUMENTARY, TV_WAR_POLITICS,WESTERN];
   static const movieTvGroup5 = [DOCUMENTARY, WAR, WESTERN, HISTORY];
 
   static const movieTvGroup6 = [TV_SOAP, TV_KIDS_GENRE, TV_REALITY, TV_TALK, TV_NEWS];
@@ -62,3 +62,4 @@ const String TV_REALITY = "Reality";
 const String TV_TALK = "Talk";
 const String TV_SOAP = "Soap";
 const String TV_WAR_POLITICS = "War & Politics";
+const String TV_SCI_FI_AND_FANTASY = "Sci-Fi & Fantasy";
